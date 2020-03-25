3ac8bfad4d83bfe6181e43362665b77895c30e10
Dominic Montelongo <montelongodominic@gmail.com>
2020-03-25 11:59:03 -0700

    Add multiply input

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 //Business (or back-end) logic:
 
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
+var add = function(number1, number2) {
+  return number1 + number2;
+};
 
 var subtract = function(number1, number2) {
-  return number1 - number2;
+  return number3 - number4;
 };
 
 var multiply = function(number1, number2) {
@@ -23,6 +23,8 @@ $(document).ready(function() {
     event.preventDefault();
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
+    var number3 = parseInt($("#subract1").val());
+    var number4 = parseInt($("#subract2").val());
     var result = add(number1, number2);
     $("#output").text(result);
   });
